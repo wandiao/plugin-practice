95ce63fa8e500354c542f448381ce2c38bf9248b
wanjie <851298395@qq.com>
2019-06-25 15:37:13 +0800

    调整引入方式

--- src/packages/react-router-dom/index.js
+++ src/packages/react-router-dom/index.js
@@ -1,13 +1,13 @@
 import BrowserRouter from './BrowserRouter'
 import HashRouter from './HashRouter'
-import { Route } from '@/react-router'
 import Link from './Link'
+
+export * from "@/react-router"
 
 
 
 export {
   BrowserRouter,
   HashRouter,
-  Route,
   Link,
 }
